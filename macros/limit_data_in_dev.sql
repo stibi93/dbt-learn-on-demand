f30d44bbab0b02491f4763c37a09c08a239eed50
rijjok@foxiomail.com <rijjok@foxiomail.com>
2021-11-16 13:21:19 +0000

    packages

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,7 +1,7 @@
- {% macro limit_data_in_dev(column_name, dev_days_of_data=3) %}
+{% macro limit_data_in_dev(column_name, dev_days_of_data=3) %}
 
-{% if target.name == 'dev' %}
-    where {{ column_name }} >= dateadd('day', -{{ dev_days_of_data }}, current_timestamp)
-{% endif % }
+    {% if target.name == 'dev' %}
+        where {{ column_name }} >= dateadd('day', -{{ dev_days_of_data }}, current_timestamp)
+    {% endif %}
 
 {% endmacro %}
